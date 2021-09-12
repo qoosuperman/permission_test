045742dbe17b367321c1bda7b45d3170a730c4c5
Anthony Chao <qoosuperman@gmail.com>
2021-09-12 14:23:19 +0800

    feat: add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(id: 1, name: 'A', email: 'AAA@gmail.com', password: '12345678')
+User.create(id: 2, name: 'B', email: 'BBB@gmail.com', password: '12345678')
+
+Book.create(user_id: 1, name: 'A book')
+Book.create(user_id: 2, name: 'B book')
